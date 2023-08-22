e5f35479710c5462fcdfd7eb69cdb459dce0bf0e
Thanushreeks35 <thanushreeks.xworkz@gmail.com>
2023-08-22 11:50:59 +0530

    committing amazon code

---
+++ AmazonPrime.java
@@ -0,0 +1,12 @@
+class AmazonPrime {
+	static String englishMovies[]= {"Gladiator", "Schindler's", "The Shawshank Redemption", "The Good father", "Interstellar", "The Matrix", "Psycho", "Pulp Fiction", "The Prestige", "Avengers", "Avatar", "WonderWomen", "Citizen"};
+	public static void main(String movie[]){
+		System.out.println("Main Started");
+		System.out.println("The list of Movies are:");
+		for(int prime=0 ; prime<englishMovies.length ; prime++){
+			System.out.println(englishMovies[prime]);
+			
+		}
+		System.out.println("Main Endes");
+	} 
+}
